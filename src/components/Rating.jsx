53e6a7002db2628d8a475971f9922ea49239648b
Maddy1305 <maddy13072@gmail.com>
2024-05-19 10:44:46 +0530

    Added the project

---
+++ src/components/Rating.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Rating = () => {
+  return (
+    <div className='rating'>
+        <span><i className='icofont-ui-rating'></i></span>
+        <span><i className='icofont-ui-rating'></i></span>
+        <span><i className='icofont-ui-rating'></i></span>
+        <span><i className='icofont-ui-rating'></i></span>
+        <span><i className='icofont-ui-rating'></i></span>
+    </div>
+  )
+}
+
+export default Rating
---
+++ src/components/Sidebar/GoogleMap.jsx
@@ -0,0 +1,11 @@
+const GoogleMap = () => {
+    return (
+        <div className="map-area">
+            <div className="maps">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15551.629138731616!2d80.22096644999999!3d12.977781599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1700118462704!5m2!1sen!2sin"></iframe>
+            </div>
+        </div>
+    );
+}
+ 
+export default GoogleMap;
